01e6ffb040a71823dbeb406e29154ee6f0b5d93b
thedevilsvoice <franklin@bitsmasher.net>
2018-01-20 12:52:40 -0500

    inital script to convert markdown to pdf

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 
 This is a collaborative effort. The goal is to collect recipes for an eclectic mix of dishes from around the hacker community. Tell us about your favorite recipe or something you like to make with your own unique twist. Feel free to include a recommendation for booze pairing.
 
-General questions should be directed to [@mzbat](https://twitter.com/mzbat) who will be curating the book along with [@iheartmalware](https://twitter.com/iheartmalware). 
+General questions should be directed to [@mzbat](https://twitter.com/mzbat) or [@iheartmalware](https://twitter.com/iheartmalware). 
+
 We can't promise that all submissions will be included in the final printed book. We do plan to leave this repo up indefinitely. We intend to give proceeds from any sales of printed book to [Electronic Frontier Foundation](https://supporters.eff.org/shop).
 
-#### Rules for Submission
+#### Guidelines for Submission
 
 1. Fork this repository. Create a subdirectory for your recipe. Try to make the name of the subdirectory match the name of your recipe.
 2. Include a .md document in your sub dir [Github Markdown Cheatsheet] (https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) You can use the [template file](https://github.com/theDevilsVoice/1337-Noms-The-Hacker-Cookbook/blob/master/template.md) as a guide to help you in your effort. There is a [markdown tutorial here](http://www.markdowntutorial.com/)
@@ -19,27 +20,32 @@ NOTE: You are releasing the rights to your work under the [included LICENSE](htt
 
 #### Who You Are
 
-Include how you’d like to be credited/identified in your .md file. 
-Include a way for us to contact you if needed.
+* Include how you’d like to be credited/identified in your .md file. 
+* Include a way for us to contact you if needed.
+* Be sure you add your details to the end of the [credits.md](https://github.com/theDevilsVoice/1337-Noms-The-Hacker-Cookbook/blob/master/credits.md) file.
 
 #### Name of dish
 
-Just like it sounds, we have to call the dish something. 
+* Just like it sounds, we have to call the dish something. 
+    * Choose a category (also OK to add a new category)
+    * Create a folder to hold your files. 
+    * Name it something like "myname_name_of_dish"
 
 #### Ingredients
 
-Please include Metric and English wherever possible
-
-[Conversion to/from English/Metric Measurements] (http://www.sciencemadesimple.com/volume_conversion.php)
-
-#### Detailed preparation instructions 
-
-Set of steps that explain how to create your dish.
+* Please include Metric and English wherever possible
+    * [Conversion to/from English/Metric Measurements] (http://www.sciencemadesimple.com/volume_conversion.php)
+* Make a file with the same format as the parent folder, but with the .md extension
+    * "myname_name_of_dish.md"
+* Set of steps that explain how to create your dish go in your .md file.
 
 #### Photos 
 
-Instructional and/or completed dish should be uploaded to your sub-folder in the repository.
+* Instructional and/or completed dish should be uploaded to your sub-folder in the repository.
 
 #### How should the Dish Be served?
 
-Temperature, serving tips, anything that gives it the finishing touch. 
+* Temperature
+* Serving tips
+* Beer, wine or drink pairings. 
+* Anything that gives it the finishing touch. 
---
+++ book/makebook.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+#
+# Author: @theDevilsVoice
+#
+# Date: 10/13/2017
+#
+# Script Name: makebook.sh
+#
+# Description: Use this shell script to ensure your system
+#              is ready for the class.
+#
+# Run Information:
+#
+# Error Log: Any output found in /path/to/logfile
+
+BUILD_DIR=/tmp/cookbook
+
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
+
+RED='\033[0;31m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+CYAN='\033[0;36m'
+LPURP='\033[1;35m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+function remove_stale {
+
+  if [ -d "${BUILD_DIR}.old" ]
+  then 
+    rm -rf ${BUILD_DIR}.old
+  fi 
+
+  if [ -d "${BUILD_DIR}" ] 
+  then 
+    mv ${BUILD_DIR} ${BUILD_DIR}.old
+    echo "Renaming stale directory in /tmp."
+  fi
+  return 0
+
+  mkdir ${BUILD_DIR}
+
+} # //remove_stale
+
+function check_installed() {
+ 
+  PACKAGE=$1
+
+  if [ $(dpkg-query -W -f='${Status}' ${PACKAGE} 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    echo -e "${LPURP}Installing package: ${PACKAGE}"
+    echo -e "${NC}"
+    sudo apt-get -y install ${PACKAGE}
+  else
+    echo -e "${LPURP}Found package: ${PACKAGE}"
+    echo -e "${NC}"
+  fi
+ 
+  return 0
+} # //check_installed 
+
+#############################
+# Do stuff for Debian based #
+#############################
+function debian {
+
+  BASE_DIR=${PWD}
+  THEMES_DIR="${BUILD_DIR}/local/lib/python2.7/site-packages/markdown2pdf/themes"
+
+  # Python3
+  check_installed python3-venv 
+  check_installed python3-dev
+  
+  #/usr/bin/python3 -m venv ${BUILD_DIR}
+  /usr/bin/python3 ./makebook.py 
+
+  if [ -f "${BUILD_DIR}/output.md" ]
+  then
+    echo -e "${LGREEN}"
+    echo "Successfully generated Markdown file"
+    echo -e "${NC}"
+  else
+    echo -e "${YELLOW}"
+    echo "Cannot find output.md, check makebook.py"
+    echo -e "${NC}"
+    exit 1
+  fi
+  # Python2
+  pip install virtualenvwrapper
+  /usr/bin/python -m virtualenv ${BUILD_DIR}
+
+  # needed for md2pdf
+  check_installed libffi-dev
+
+  cd ${BUILD_DIR} && source bin/activate  
+
+  #
+  # https://pypi.python.org/pypi/Markdown2PDF/0.1.3
+  #
+  pip install wheel
+  pip install markdown2pdf
+  if [ -d "${THEMES_DIR}" ]
+  then 
+    echo -e "${LPURP}"
+    echo "Found themes directory"
+    echo -e "${NC}"
+  else 
+    echo -e "${LPURP}"
+    echo "Creating themes directory, copying themes"
+    echo -e "${NC}"
+    mkdir ${THEMES_DIR}
+    cp ${BASE_DIR}/github.css ${THEMES_DIR}
+  fi
+
+  if [ -f "${BUILD_DIR}/output.md" ] 
+  then 
+    echo -e "${LPURP}"
+    echo "Building PDF..."
+    echo -e "${NC}"
+    md2pdf ${BUILD_DIR}/output.md
+    #md2pdf ${BUILD_DIR}/output.md --theme=github 
+    #md2pdf output.md --theme=path_to_style.css
+  else 
+    echo -e "${YELLOW}" 
+    echo "Cannot find output.md, check makebook.py"  
+    echo -e "${NC}"
+    exit 1
+  fi 
+ 
+  if [ -f "${BUILD_DIR}/output.pdf" ] 
+  then 
+    mv ${BUILD_DIR}/output.pdf ${BASE_DIR}/hacker_cookbook.pdf
+    echo -e "${LGREEN}"
+    echo "Success!"
+    echo -e "${NC}"
+  else 
+    echo -e "${YELLOW}"
+    echo "Could not find ${BUILD_DIR}/output.pdf"
+    echo -e "${NC}"
+    exit 1
+  fi
+  #cd ${BASE_DIR} && rm -rf ${BUILD_DIR}
+  deactivate 
+  return 0
+
+} # //debian()
+
+#######################
+# Do stuff for RedHat #
+#######################
+function redhat {
+
+  return 0
+}
+
+########################
+# Do stuff for FreeBSD #
+########################
+
+########################
+# Do stuff for OpenBSD #
+########################
+function obsd {
+
+  return 0
+}
+
+######################
+# Do Stuff for Apple #
+######################
+function apple {
+  brew install cairo pango gdk-pixbuf libxml2 libxslt libffi
+  return 0
+}
+
+function main {
+
+  remove_stale 
+
+  if [ "$(uname)" == "Darwin" ]; then
+    apple
+  elif [ "$(uname)" == "OpenBSD" ]; then
+    obsd
+  elif [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
+    redhat
+  elif [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+    debian
+  else
+    echo "Unable to run on this architecture"
+    exit 1
+  fi
+
+} # //main()
+
+if [ -z "$ARGS" ] ; then
+  main $@
+else
+  main $ARGS
+fi

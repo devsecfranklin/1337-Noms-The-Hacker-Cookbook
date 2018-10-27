6c987df28a5948e230d2aadbbce812890ca1aa42
FrankTheTank <franklin@bitsmasher.net>
2018-10-27 16:07:00 -0400

    testing

--- build/success.sh
+++ build/success.sh
@@ -21,7 +21,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
    # hey that's a pull request
-   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+   curl -H "Authorization: token ${GH_TOKEN}" -X POST \
 	   -d "{\"body\": \"PR looks good. An admin will merge soon.\"}" \
 	   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi

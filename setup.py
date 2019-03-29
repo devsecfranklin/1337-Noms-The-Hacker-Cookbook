d725c05711cf803bce954e2f55821d7eca56bcc5
FrankTheTank <franklin@bitsmasher.net>
2019-03-28 21:19:05 -0400

    change css

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1',
     packages=['hacker_cookbook', 'makebook'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
